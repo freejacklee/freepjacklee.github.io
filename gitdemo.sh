129bbe31b66669a01cea79dc10c6324585d8117b
Victor Lin <victorlin1983@outlook.com>
2022-02-05 21:20:43 -0500

    verify crontab information

--- gitdemo.sh
+++ gitdemo.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
+
 #This program auto execute
 #git command
 
-cd /Users/linvictor/githubpage
+cd /Users/linvictor/githubpage/jacklee
 git add .
-git commit -m “verify crontab information”
-git push origin/master
+git commit -m "verify crontab information"
+git push
 
